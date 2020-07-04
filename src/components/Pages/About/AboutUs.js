198966a46a69b1b750d2b01de5efa607aeb5354a
Rohit Kumar Bisht <63202878+rohitkumarbisht@users.noreply.github.com>
2020-07-04 15:05:52 +0530

    Add files via upload

---
+++ src/components/Pages/About/AboutUs.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class AboutUs extends Component {
+  render() {
+    return (
+      <div>
+        {" "}
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-4">About FindMyEvent</h1>
+            <hr></hr>
+            <h3>Simple app to manage events</h3>
+            <p className="text-secondary">Version 1.0.0</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default AboutUs;
